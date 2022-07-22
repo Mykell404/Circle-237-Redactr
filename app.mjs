31d95c630e86efa067d51f5d25336521f1aeb5a9
Kingsley Nwankwo <108965514+kingsbloc@users.noreply.github.com>
2022-07-22 14:31:16 +0000

    added function to redactWords

--- app.mjs
+++ app.mjs
@@ -1,23 +1,49 @@
-function startApp() {
-  let redactBtn = document.getElementById("button");
+function startApp(e) {
+  // Get the Form Element
+  let redactForm = document.getElementById("redact-form");
+
   // This function splits the words and returns an array
   function sortWords(words) {
-    let newWords = words.split(",");
-    return newWords;
+    // convert the string to an array then map through the words to remove space at the start and end of the word
+    let newWords = words.split(",").map((w) => w.trim());
+
+    // return a unique list of the newWords to prevent duplicate entries
+    return [...new Set(newWords)];
   }
 
-  function redact(event) {
+  function redactWords(wordList, content, symbol) {
+    return new Promise(resolve => {
+      // we assign the content here so it can be updated
+      let scrambledContent = content;
+      // we iterate through the wordlist and replace all words with the symbol
+      wordList.forEach((w) => {
+        scrambledContent = scrambledContent.replaceAll(w, symbol)
+      })
+      // we let the Promise know that we are done and resolve with the result
+      resolve(scrambledContent);
+    })
+  }
+
+  async function redact(event) {
+    // Prevent the default action of the form ie the reloading
     event.preventDefault();
-    let contentToBeScrambled = document.getElementById("text").value;
-    let wordsToBeScrambled = document.getElementById("scramble").value;
-    let symbol = document.getElementById("symbol");
+    // Get value from the text input field
+    let contentToBeScrambled = redactForm.text.value;
+    // Get value from the scramble input field
+    let wordsToBeScrambled = redactForm.scramble.value;
+    // Get value from the symbol input field
+    let symbol = redactForm.symbol.value;
 
     let sortedWord = sortWords(wordsToBeScrambled);
+    // pass the sorted words, content and symbol then await the result
+    let result = await redactWords(sortedWord, contentToBeScrambled, symbol)
 
-    console.log(sortedWord);
+    // console.log the result
+    console.log(result);
   }
 
-  redactBtn.addEventListener("click", redact);
+  // Listen for sumbit event on the redact Form
+  redactForm.addEventListener("submit", redact)
 };
 
 // ======= DO NOT EDIT ============== //
--- index.mjs
+++ index.mjs
@@ -1,3 +1,27 @@
 import startApp from './app.mjs';
 
-document.addEventListener('DOMContentLoaded', startApp); 
+document.addEventListener('DOMContentLoaded', startApp);
+
+const form = document.getElementById('redact-form');
+const text = document.getElementById('text');
+const scramble = document.getElementById('scramble');
+const symbol = document.getElementById('symbol');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+};
+
+const validateInputs = () => {
+  const textValue = text.value.trim();
+  const scrambleValue = scramble.value.trim();
+  const symbolValue = symbol.value.trim();
+};
