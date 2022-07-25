591092cf46951fb290ee0749fa0e414b1d5203f9
Nana Kwame Ogyire <104514687+stardev1@users.noreply.github.com>
2022-07-25 08:30:31 +0000

    Updates the redact function to display the redacted text on the UI. Updates UI text to make it more user-friendly.

--- app.mjs
+++ app.mjs
@@ -17,6 +17,8 @@ function startApp(e) {
     return new Promise(resolve => {
       // we assign the content here so it can be updated
       let scrambledContent = content;
+
+      // the below function needs to be rewritten, it replaces characters (not words) with symbols throughout the entire text. for example, if the text is "I want to be the best" and the word to be scrambled is "be" and the symbol is "?", the result is "I want to ? the ?st" (nana) 
       // we iterate through the wordlist and replace all words with the symbol
       wordList.forEach((w) => {
         scrambledContent = scrambledContent.replaceAll(w, symbol)
@@ -44,20 +46,21 @@ function startApp(e) {
 
     let totalWordCount = contentToBeScrambled.length;
     let scrambledWordCount = result.length;
-
+    
+    let redactedTextNode = document.getElementById("redacted-text");
     let wordCountNode = document.getElementById("word-count");
-    let scrambledWordCountNode = document.getElementById(
-      "scrambled-word-count"
-    );
+    let scrambledWordCountNode = document.getElementById("scrambled-word-count");
 
-    wordCountNode.innerText = `Words Count: ${totalWordCount}`;
-    scrambledWordCountNode.innerText = `Scrambled words Count: ${scrambledWordCount}`;
+    // changed Words count to Initial Text Character count which is more accurate (nana)
+    wordCountNode.innerText = `Initial Text Character Count: ${totalWordCount}`;
 
-    // console.log the result
-    console.log(result);
+    // changed Scrambled words Count to Redacted Text Character Count which is more accurate. We should also update the code to correctly count words if we want to display a word count (nana)
+    scrambledWordCountNode.innerText = `Redacted Text Character Count: ${scrambledWordCount}`;
+    redactedTextNode.innerText = `Redacted Text: ${result}`
+
   }
 
-  // Listen for sumbit event on the redact Form
+  // Listen for submit event on the redact Form
   redactForm.addEventListener("submit", redact)
 };
 
